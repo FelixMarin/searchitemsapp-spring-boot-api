3b8d76c28a1b202f2fa4e78b0038ed786e9ca4ca
Félix Marín Ramírez <felixmurcia@gmail.com>
2020-07-16 20:03:15 +0200

    "se añaden nuevos tags"

--- src/main/java/com/searchitemsapp/controller/ApplicationController.java
+++ src/main/java/com/searchitemsapp/controller/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,7 @@ import com.searchitemsapp.services.IFServiceFctory;
  * @author Felix Marin Ramirez
  *
  */
+@CrossOrigin(origins = { "http://localhost:8081" }, allowCredentials = "true")
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 @RestController
 public class ApplicationController {
